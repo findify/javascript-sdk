37601f5bd11cf5cef1b3106beac598798ce43e91
Ivan Mironchik <me.roaiven@gmail.com>
2016-12-21 17:26:29 +0300

    Fix typo

--- src/utils/resolveUrl.ts
+++ src/utils/resolveUrl.ts
@@ -1,8 +1,8 @@
 function resolveUrl(root, pathname) {
-  const rootWihoutTrailingSlash = root.slice(-1) !== '/' ? root : root.slice(0, -1);
+  const rootWithoutTrailingSlash = root.slice(-1) !== '/' ? root : root.slice(0, -1);
   const pathnameWithoutLeadingSlash = pathname[0] !== '/' ? pathname : pathname.slice(1);
 
-  return rootWihoutTrailingSlash + '/' + pathnameWithoutLeadingSlash;
+  return rootWithoutTrailingSlash + '/' + pathnameWithoutLeadingSlash;
 }
 
 export {
