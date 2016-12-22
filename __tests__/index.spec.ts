231d9eb1f266789334759680a6c0e6c62a9b6686
Ivan Mironchik <me.roaiven@gmail.com>
2016-12-22 19:23:17 +0300

    Implement sdk class with autocomplete method

---
+++ __tests__/index.spec.ts
@@ -0,0 +1,252 @@
+import * as fauxJax from 'faux-jax';
+import * as expect from 'expect';
+import * as assign from 'lodash/assign';
+import * as omit from 'lodash/omit';
+import { setupJsDom, teardownJsDom } from './jsdom-helper';
+
+import FindifySDK from '../src/index';
+
+describe('FindifySDK', () => {
+  beforeEach(() => {
+    fauxJax.install();
+  });
+
+  afterEach(() => {
+    fauxJax.restore();
+  });
+
+  const key = 'testApiKey';
+  const user = {
+    uid: 'testUserId',
+    sid: 'testSessionId',
+  };
+
+  describe('generic', () => {
+    it('should be instantiated', () => {
+      const sdk = new FindifySDK({ key });
+    });
+  });
+
+  describe('autocomplete', () => {
+    it('should use jsonp by default if "method" is not provided at config', (done) => {
+      setupJsDom(() => {
+        fauxJax.on('request', (req) => {
+          expect(req.requestMethod).toBe('GET');
+          teardownJsDom();
+          done();
+        });
+
+        const sdk = new FindifySDK({ key });
+
+        sdk.autocomplete({
+          q: 'test',
+          user,
+        });
+      });
+    });
+
+    it('should use jsonp if { method: "jsonp" } is provided', (done) => {
+      setupJsDom(() => {
+        fauxJax.on('request', (req) => {
+          expect(req.requestMethod).toBe('GET');
+          teardownJsDom();
+          done();
+        });
+
+        const sdk = new FindifySDK({
+          key,
+          method: 'jsonp',
+        });
+
+        sdk.autocomplete({
+          q: 'test',
+          user,
+        });
+      });
+    });
+
+    it('should use POST if { method: "post" } is provided', (done) => {
+      fauxJax.on('request', (req) => {
+        expect(req.requestMethod).toBe('POST');
+        done();
+      });
+
+      const sdk = new FindifySDK({
+        key,
+        method: 'post',
+      });
+
+      sdk.autocomplete({
+        q: 'test',
+        user,
+      });
+    });
+
+    it('should throw an Error if "user" param is not provided neither at configuration nor in request', () => {
+      const sdk = new FindifySDK({ key });
+      expect(() => sdk.autocomplete({
+        q: 'test',
+      })).toThrow(/`user` param should be provided at request or at library config/);
+    });
+
+    it('should add "user" param to request body if it`s provided at sdk initialization', (done) => {
+      fauxJax.on('request', (req) => {
+        const requestBody = JSON.parse(req.requestBody);
+
+        expect(requestBody.user).toBeA('object');
+
+        done();
+      });
+
+      const sdk = new FindifySDK({
+        key,
+        method: 'post',
+        user,
+      });
+
+      sdk.autocomplete({
+        q: 'test',
+      });
+    });
+
+    it('should add "user" param to request body if it`s provided as a request method param', (done) => {
+      fauxJax.on('request', (req) => {
+        const requestBody = JSON.parse(req.requestBody);
+
+        expect(requestBody.user).toBeA('object');
+
+        done();
+      });
+
+      const sdk = new FindifySDK({
+        key,
+        method: 'post',
+      });
+
+      sdk.autocomplete({
+        q: 'test',
+        user,
+      });
+    });
+
+    it('should add "t_client" param to request body', (done) => {
+      fauxJax.on('request', (req) => {
+        const requestBody = JSON.parse(req.requestBody);
+
+        expect(requestBody.t_client).toBeA('number');
+
+        done();
+      });
+
+      const sdk = new FindifySDK({
+        key,
+        method: 'post',
+        user,
+      });
+
+      sdk.autocomplete({
+        q: 'test',
+      });
+    });
+
+    it('should add "log" param to request body if it`s provided at sdk initialization', (done) => {
+      fauxJax.on('request', (req) => {
+        const requestBody = JSON.parse(req.requestBody);
+
+        expect(requestBody.log).toBe(true);
+
+        done();
+      });
+
+      const sdk = new FindifySDK({
+        key,
+        method: 'post',
+        user,
+        log: true,
+      });
+
+      sdk.autocomplete({
+        q: 'test',
+      });
+    });
+
+    it('should add passed request params to request body', (done) => {
+      const request = {
+        q: 'test',
+        suggestion_limit: 10,
+        item_limit: 15,
+      };
+
+      fauxJax.on('request', (req) => {
+        const requestBody = omit(JSON.parse(req.requestBody), ['t_client']);
+
+        expect(requestBody).toEqual(assign({}, {
+          user,
+        }, request));
+
+        done();
+      });
+
+      const sdk = new FindifySDK({
+        key,
+        method: 'post',
+        user,
+      });
+
+      sdk.autocomplete(request);
+    });
+
+    it('should provide "key" param to headers', (done) => {
+      fauxJax.on('request', (req) => {
+        expect(req.requestHeaders['x-key']).toBe(key);
+        done();
+      });
+
+      const sdk = new FindifySDK({
+        key,
+        method: 'post',
+        user,
+      });
+
+      sdk.autocomplete({
+        q: 'test',
+      });
+    });
+
+    it('should add jsonp callback prefix as "findifyCallback"', (done) => {
+      setupJsDom(() => {
+        fauxJax.on('request', (req) => {
+          expect(req.requestURL.indexOf('findifyCallback') > -1).toBe(true);
+          teardownJsDom();
+          done();
+        });
+
+        const sdk = new FindifySDK({
+          key,
+          user,
+        });
+
+        sdk.autocomplete({
+          q: 'test',
+        });
+      });
+    });
+
+    it('it should send requests to "https://api-v3.findify.io"', (done) => {
+      fauxJax.on('request', (req) => {
+        expect(req.requestURL.indexOf('https://api-v3.findify.io') > -1).toBe(true);
+        done();
+      });
+
+      const sdk = new FindifySDK({
+        key,
+        method: 'post',
+        user,
+      });
+
+      sdk.autocomplete({
+        q: 'test',
+      });
+    });
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as assign from 'lodash/assign';
+
+import { requestApi, Config as RequestApiConfig } from './utils/requestApi';
+
+class SDK {
+  private config: FindifySDK.Config;
+  private requestApiConfig: RequestApiConfig;
+
+  private makeExtendedRequest(request: Request) {
+    return assign({}, {
+      user: this.config.user,
+      log: this.config.log,
+      t_client: (new Date()).getTime(),
+    }, request);
+  }
+
+  private makeRequestApiConfig() {
+    return {
+      host: 'https://api-v3.findify.io',
+      jsonpCallbackPrefix: 'findifyCallback',
+      method: this.config.method || 'jsonp',
+      key: this.config.key,
+    };
+  }
+
+  private throwUserArgError() {
+    throw new Error('`user` param should be provided at request or at library config');
+  }
+
+  public constructor(config: FindifySDK.Config) {
+    this.config = config;
+    this.requestApiConfig = this.makeRequestApiConfig();
+  }
+
+  public autocomplete(request: FindifySDK.AutocompleteRequest) {
+    const extendedRequest = this.makeExtendedRequest(request);
+
+    if (!extendedRequest.user) {
+      this.throwUserArgError();
+    }
+
+    return requestApi('/autocomplete', extendedRequest, this.requestApiConfig);
+  }
+}
+
+type ExtendedRequest<Request> = Request & {
+  user: FindifySDK.User,
+  t_client: number,
+  log?: boolean,
+};
+
+type Request = FindifySDK.AutocompleteRequest;
+
+export default SDK;
