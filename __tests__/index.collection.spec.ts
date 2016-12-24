0011fd1af5de9267464f6dad99b1eb51d6eec0a2
Ivan Mironchik <me.roaiven@gmail.com>
2016-12-24 13:44:07 +0300

    Remove extra tests

--- __tests__/index.collection.spec.ts
+++ __tests__/index.collection.spec.ts
@@ -4,24 +4,6 @@ import * as assign from 'lodash/assign';
 import * as omit from 'lodash/omit';
 
 import {
-  jsonpRequestMethodCase,
-  jsonpByDefaultRequestMethodCase,
-  postRequestMethodCase,
-  userParamValidationCase,
-  userParamRequestBodyAtConfigurationCase,
-  userParamRequestBodyAtRequestCase,
-  timestampAddingCase,
-  logConfigParamCase,
-  keyParamsInHeadersCase,
-  jsonpCallbackPrefixCase,
-  apiHostnameCase,
-  uidParamExistanceAtConfigurationCase,
-  uidParamExistanceAtRequestCase,
-  sidParamExistanceAtConfigurationCase,
-  sidParamExistanceAtRequestCase,
-} from './generic-helper';
-
-import {
   filterNameParamExistanceCase,
   filterTypeParamExistanceCase,
   sortFieldParamExistanceCase,
@@ -75,22 +57,6 @@ describe('collection', () => {
   afterEach(() => {
     fauxJax.restore();
   });
-
-  it('should use jsonp by default if "method" is not provided at config', jsonpByDefaultRequestMethodCase(makeSearch));
-  it('should use jsonp if { method: "jsonp" } is provided', jsonpRequestMethodCase(makeSearch));
-  it('should use POST if { method: "post" } is provided', postRequestMethodCase(makeSearch));
-  it('should throw an Error if "user" param is not provided neither at configuration nor in request', userParamValidationCase(makeSearch));
-  it('should add "user" param to request body if it`s provided at sdk initialization', userParamRequestBodyAtConfigurationCase(makeSearch));
-  it('should add "t_client" param to request body', timestampAddingCase(makeSearch));
-  it('should add "log" param to request body if it`s provided at sdk initialization', logConfigParamCase(makeSearch));
-  it('should provide "key" param to headers', keyParamsInHeadersCase(key, makeSearch));
-  it('should add jsonp callback prefix as "findifyCallback"', jsonpCallbackPrefixCase(makeSearch));
-  it('should send requests to "https://api-v3.findify.io"', apiHostnameCase(makeSearch));
-  it('should add "user" param to request body if it`s provided as a request method param', userParamRequestBodyAtRequestCase(user, makeSearchWithUser));
-  it('should throw validation Error if "user.uid" param is not provided at library configuration', uidParamExistanceAtConfigurationCase(makeSearch));
-  it('should throw validation Error if "user.sid" param is not provided at library configuration', sidParamExistanceAtConfigurationCase(makeSearch));
-  it('should throw validation Error if "user.uid" param is not provided at request', uidParamExistanceAtRequestCase(makeSearchWithSid));
-  it('should throw validation Error if "user.sid" param is not provided at request', sidParamExistanceAtRequestCase(makeSearchWithUid));
 
   it('should add passed request params to request body', (done) => {
     const request = {
