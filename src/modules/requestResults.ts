6eded275ed33495e40cc4172ecd5b0dd3ab38a18
Ivan Mironchik <me.roaiven@gmail.com>
2016-12-24 14:46:04 +0300

    Implement "requestResults" module

--- src/modules/requestResults.ts
+++ src/modules/requestResults.ts
@@ -1,10 +1,36 @@
-import { extendRequest, ConfigData } from './extendRequest';
+import * as every from 'lodash/every';
+import * as curry from 'lodash/curry';
 
-function requestResults(endpoint: string, request: Request, configData: ConfigData) {
+import { extendRequest } from './extendRequest';
+import { requestApi } from './requestApi';
 
+function requestResults(endpoint: string, request: FindifySDK.ResultsRequest, config: FindifySDK.Config) {
+  const { filters, sort } = request;
+
+  if (filters && !everyKey(filters, 'name')) {
+    throw new Error('"filters.name" param is required');
+  }
+
+  if (filters && !everyKey(filters, 'type')) {
+    throw new Error('"filters.type" param is required');
+  }
+
+  if (sort && !everyKey(sort, 'field')) {
+    throw new Error('"sort.field" param is required');
+  }
+
+  if (sort && !everyKey(sort, 'order')) {
+    throw new Error('"sort.order" param is required');
+  }
+
+  const extendedRequest = extendRequest(request, config);
+
+  return curry(requestApi)(endpoint, extendedRequest);
 }
 
-type Request = FindifySDK.SearchRequest | FindifySDK.CollectionRequest;
+function everyKey(c, key) {
+  return every(c, (item) => typeof item[key] !== 'undefined');
+}
 
 export {
   requestResults,
